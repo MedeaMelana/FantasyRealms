29ec1f8ed6da88a2b57804b07e610b33b527292b
Martijn van Steenbergen <martijn@van.steenbergen.nl>
2024-05-22 21:18:46 +0100

    Inline mkCard

--- src/FantasyRealms.hs
+++ src/FantasyRealms.hs
@@ -1,15 +1,11 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE LambdaCase #-}
--- {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE RecordWildCards #-}
 
 module FantasyRealms where
 
 import Data.Boolean (notB, (&&*), (||*))
-import Data.Function ((&))
 import Data.Map (Map)
 import Data.Map qualified as Map
--- import Data.Monoid (Sum (..))
 import Data.Set (Set)
 import Data.Set qualified as Set
 
@@ -52,6 +48,9 @@ type Hand = Set CardName
 
 type Modifier = Hand -> Int
 
+noModifier :: Modifier
+noModifier _ = 0
+
 data Card = Card
   { name :: String,
     baseStrength :: Int,
@@ -61,21 +60,6 @@ data Card = Card
   }
 
 type Predicate a = a -> Bool
-
-mkCard :: Suit -> String -> Int -> Card
-mkCard suit name baseStrength =
-  Card {bonus = const 0, penalty = const 0, ..}
-
-withBonusWhen :: Int -> Predicate Hand -> Card -> Card
-withBonusWhen score matches = withBonus (when matches score)
-
-withBonus :: Modifier -> Card -> Card
-withBonus score card =
-  card {bonus = \hand -> bonus card hand + score hand}
-
-withPenalty :: Modifier -> Card -> Card
-withPenalty score card =
-  card {penalty = \hand -> penalty card hand + score hand}
 
 isValid :: Predicate Hand
 isValid hand = Set.size hand == requiredSize
@@ -97,71 +81,135 @@ hasName cardName card = name (describe cardName) == name card
 describe :: CardName -> Card
 describe = \case
   BellTower ->
-    mkCard Land "Bell Tower" 8
-      & withBonusWhen 15 (hasCardThat (hasSuit Wizard))
-  BookOfChanges -> mkCard Artifact "Book of Changes" 3 -- TODO
+    Card
+      { name = "Bell Tower",
+        suit = Land,
+        baseStrength = 8,
+        bonus = when (hasCardThat (hasSuit Wizard)) 15,
+        penalty = noModifier
+      }
+  BookOfChanges ->
+    Card
+      { name = "Book of Changes",
+        suit = Artifact,
+        baseStrength = 3,
+        bonus = noModifier, -- TODO
+        penalty = noModifier
+      }
   Candle ->
-    mkCard Flame "Candle" 2
-      & withBonusWhen
-        100
-        ( hasCardThat (hasName BookOfChanges)
-            &&* hasCardThat (hasName BellTower)
-            &&* hasCardThat (hasSuit Wizard)
-        )
+    Card
+      { name = "Candle",
+        suit = Flame,
+        baseStrength = 2,
+        bonus =
+          when
+            ( hasCardThat (hasName BookOfChanges)
+                &&* hasCardThat (hasName BellTower)
+                &&* hasCardThat (hasSuit Wizard)
+            )
+            100,
+        penalty = noModifier
+      }
   Empress ->
-    mkCard Leader "Empress" 15
-      & withBonus (\hand -> 10 * Set.size (Set.filter (hasSuit Army . describe) hand))
-      & withPenalty (\hand -> (-5) * Set.size (Set.filter (hasSuit Leader . describe) hand))
-      -- TODO Empress should not count itself
+    Card
+      { name = "Empress",
+        suit = Leader,
+        baseStrength = 15,
+        bonus = \hand -> 10 * Set.size (Set.filter (hasSuit Army . describe) hand),
+        penalty =
+          \hand -> (-5) * Set.size (Set.filter (hasSuit Leader . describe) hand)
+          -- TODO Empress should not count itself
+      }
   Enchantress ->
-    mkCard Wizard "Enchantress" 5
-      & withBonus (\hand -> 10 * Set.size (Set.filter (hasElementalSuit . describe) hand))
+    Card
+      { name = "Enchantress",
+        suit = Wizard,
+        baseStrength = 5,
+        bonus = \hand -> 10 * Set.size (Set.filter (hasElementalSuit . describe) hand),
+        penalty = noModifier
+      }
     where
       hasElementalSuit = hasSuit Land ||* hasSuit Weather ||* hasSuit Flood ||* hasSuit Flame
-  King -> mkCard Leader "King" 8
-      & withBonus (\hand -> perArmyBonus hand * Set.size (Set.filter (hasSuit Army . describe) hand))
+  King ->
+    Card
+      { name = "King",
+        suit = Leader,
+        baseStrength = 8,
+        bonus = \hand -> perArmyBonus hand * Set.size (Set.filter (hasSuit Army . describe) hand),
+        penalty = noModifier
+      }
     where
       perArmyBonus hand = if Set.member Queen hand then 20 else 5
   Necromancer ->
-    mkCard Wizard "Necromancer" 3
-      & withBonusWhen 14 (hasCardThat (hasSuit Leader ||* hasSuit Wizard))
+    Card
+      { name = "Necromancer",
+        suit = Wizard,
+        baseStrength = 3,
+        bonus = when (hasCardThat (hasSuit Leader ||* hasSuit Wizard)) 14,
+        penalty = noModifier
+      }
   Princess ->
-    mkCard Leader "Princess" 2
-      & withBonus (\hand -> 8 * Set.size (Set.filter (hasSuit Leader . describe) hand))
-      -- TODO Princess should not count itself
+    Card
+      { name = "Princess",
+        suit = Leader,
+        baseStrength = 2,
+        bonus = \hand -> 8 * Set.size (Set.filter (hasSuit Leader . describe) hand),
+        penalty = noModifier
+      }
+  -- TODO Princess should not count itself
   Queen ->
-    mkCard Leader "Queen" 6
-      & withBonus (\hand -> perArmyBonus hand * Set.size (Set.filter (hasSuit Army . describe) hand))
+    Card
+      { name = "Queen",
+        suit = Leader,
+        baseStrength = 6,
+        bonus = \hand -> perArmyBonus hand * Set.size (Set.filter (hasSuit Army . describe) hand),
+        penalty = noModifier
+      }
     where
       perArmyBonus hand = if Set.member King hand then 20 else 5
   ShieldOfKeth ->
-    mkCard Artifact "Shield of Keth" 4
-      & withBonusWhen
-        15
-        ( hasCardThat (hasSuit Leader)
-            &&* notB (hasCardThat (hasName SwordOfKeth))
-        )
-      & withBonusWhen
-        40
-        (hasCardThat (hasSuit Leader) &&* hasCardThat (hasName SwordOfKeth))
+    Card
+      { name = "Shield of Keth",
+        suit = Artifact,
+        baseStrength = 4,
+        bonus = \hand ->
+          when (hasCardThat (hasSuit Leader) &&* notB (hasCardThat (hasName SwordOfKeth))) 15 hand
+            + when (hasCardThat (hasSuit Leader) &&* hasCardThat (hasName SwordOfKeth)) 40 hand,
+        penalty = noModifier
+      }
   SwordOfKeth ->
-    mkCard Weapon "Sword of Keth" 7
-      & withBonusWhen
-        10
-        ( hasCardThat (hasSuit Leader)
-            &&* notB (hasCardThat (hasName ShieldOfKeth))
-        )
-      & withBonusWhen
-        40
-        (hasCardThat (hasSuit Leader) &&* hasCardThat (hasName ShieldOfKeth))
+    Card
+      { name = "Sword of Keth",
+        suit = Weapon,
+        baseStrength = 7,
+        bonus = \hand ->
+          when (hasCardThat (hasSuit Leader) &&* notB (hasCardThat (hasName ShieldOfKeth))) 10 hand
+            + when (hasCardThat (hasSuit Leader) &&* hasCardThat (hasName ShieldOfKeth)) 40 hand,
+        penalty = noModifier
+      }
   Unicorn ->
-    mkCard Beast "Unicorn" 9
-      & withBonusWhen 30 (hasCardThat (hasName Princess))
-      & withBonusWhen 15 (notB (hasCardThat (hasName Princess)) &&*
-          hasCardThat (hasName Empress ||* hasName Queen ||* hasName Enchantress))
+    Card
+      { name = "Unicorn",
+        suit = Beast,
+        baseStrength = 9,
+        bonus = \hand ->
+          when (hasCardThat (hasName Princess)) 30 hand
+            + when
+              ( notB (hasCardThat (hasName Princess))
+                  &&* hasCardThat (hasName Empress ||* hasName Queen ||* hasName Enchantress)
+              )
+              15
+              hand,
+        penalty = noModifier
+      }
   Warhorse ->
-    mkCard Beast "Warhorse" 6
-      & withBonusWhen 14 (hasCardThat (hasSuit Leader ||* hasSuit Wizard))
+    Card
+      { name = "Warhorse",
+        suit = Beast,
+        baseStrength = 6,
+        bonus = when (hasCardThat (hasSuit Leader ||* hasSuit Wizard)) 14,
+        penalty = noModifier
+      }
 
 scoreHand :: Hand -> Map CardName Int
 scoreHand hand =
